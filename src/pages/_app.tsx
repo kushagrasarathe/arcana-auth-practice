621e7292b47125ae68f7b6a0f982d2c9af790e52
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-07-07 15:52:25 +0530

    arcana google login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+
+// clientId: Arcana Unique App Identifier via Dashboard
+const provider = new AuthProvider(`${clientId}`);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth provider={provider}>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
 }
